1b26ede8f95ab88acab944545576928b6df25f8a
Josh Justice <josh.justice@accesscfa.com>
2020-03-24 06:10:47 -0400

    Reorganize sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,33 +3,43 @@ module.exports = {
   themeConfig: {
     sidebar: [
       '/about-this-guide',
-      '/why-agile',
-      ['/why-tdd', 'Why TDD?'],
-      ['/outside-in-tdd', 'Outside-In TDD'],
-      '/exercise-intro',
       {
-        title: 'Vue',
+        title: 'Concepts',
         children: [
-          '/vue/',
-          '/vue/2-project-setup',
-          '/vue/3-vertical-slice',
-          '/vue/4-refactoring-styles',
-          '/vue/5-edge-cases',
-          '/vue/6-writing-data',
-        ],
+          '/why-agile',
+          ['/why-tdd', 'Why TDD?'],
+          ['/outside-in-tdd', 'Outside-In TDD'],
+        ]
       },
       {
-        title: 'React',
+        title: 'Exercise',
         children: [
-          '/react/',
-          '/react/2-project-setup',
-          '/react/3-vertical-slice',
-          '/react/4-refactoring-styles',
-          '/react/5-edge-cases',
-          '/react/6-writing-data',
-        ],
+          '/exercise-intro',
+          {
+            title: 'Vue',
+            children: [
+              '/vue/',
+              '/vue/2-project-setup',
+              '/vue/3-vertical-slice',
+              '/vue/4-refactoring-styles',
+              '/vue/5-edge-cases',
+              '/vue/6-writing-data',
+            ],
+          },
+          {
+            title: 'React',
+            children: [
+              '/react/',
+              '/react/2-project-setup',
+              '/react/3-vertical-slice',
+              '/react/4-refactoring-styles',
+              '/react/5-edge-cases',
+              '/react/6-writing-data',
+            ],
+          },
+          '/exercise-wrap-up',
+        ]
       },
-      '/exercise-wrap-up',
       '/next-steps',
     ],
   },
