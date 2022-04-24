b0faea8c207719b2d01aaf83107f758e544f51f5
Josh Justice <me@codingitwrong.com>
2022-04-24 07:29:27 -0400

    Remove edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,8 +26,8 @@ const config = {
           routeBasePath: 'book',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         // blog: {
         //   showReadingTime: true,
