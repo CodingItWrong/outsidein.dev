56e14cdc7d151f42ac3f28f4cb3e98ace504ecda
Josh Justice <me@codingitwrong.com>
2022-04-24 07:37:52 -0400

    Set page title and meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title="Home"
+      description="An extended tutorial in outside-in test-driven development in React and Vue."
     >
       <HomepageHeader />
       <main>
