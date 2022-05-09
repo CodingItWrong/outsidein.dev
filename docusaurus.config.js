758d24cf5503f6fc3fc408bfef6914f2b51e98d3
Josh Justice <me@codingitwrong.com>
2022-05-09 08:03:18 -0400

    Make blog disabling explicit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,12 +33,7 @@ const config = {
           // editUrl:
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
