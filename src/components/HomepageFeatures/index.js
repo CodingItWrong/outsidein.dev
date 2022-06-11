475298c176ec5d037b7bc665fca4dcfcaf6560c4
Josh Justice <me@codingitwrong.com>
2022-06-11 07:23:50 -0400

    Fix alignment of home page sections

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -35,8 +35,8 @@ const FeatureList = [
 function Feature({title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
+      <div className="padding-horiz--md">
+        <h3 className="text--center">{title}</h3>
         <p>{description}</p>
       </div>
     </div>
